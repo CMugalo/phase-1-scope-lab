456365bd28bfe5c4b82c3878b1ac8973e9d9dc9b
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-03-30 14:09:28 +0300

    adds scope lab code

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+console.log(customerName);
+
+function upperCaseCustomerName() {
+  if ((customerName = "bob")) {
+    return (customerName = "BOB");
+  }
+}
+
+customerName();
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+  return bestCustomer;
+}
+
+bestCustomer();
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+  return bestCustomer;
+}
+
+bestCustomer();
+
+const leastFavoriteCustomer = "Samuel";
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = "Samuel";
+  leastFavoriteCustomer = "David";
+  return leastFavoriteCustomer;
+}
+
+changeLeastFavoriteCustomer();
